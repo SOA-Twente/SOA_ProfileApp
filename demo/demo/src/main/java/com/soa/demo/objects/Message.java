637c8673eecee05783136b735067312bde680372
Floris <floris.kromwijk@gmail.com>
2023-03-23 01:39:53 +0100

    Implementation of Profile base

--- demo/demo/src/main/java/com/soa/demo/objects/Message.java
+++ demo/demo/src/main/java/com/soa/demo/objects/Message.java
@@ -2,15 +2,45 @@ package com.soa.demo.objects;
 
 public class Message {
 
+    private int id;
+    private int userId;
     private String message;
+    private boolean isReply;
+    private int repliedQuackId;
+    private boolean isRetweet;
+    private int retweetedQuackId;
+    private String createdAt;
 
-    public Message(){
-
-    }
-    public Message(String message) {
+    //NOSONAR
+    public Message(int id, String message, int userId, boolean isReply, int repliedQuackId, boolean isRetweet, int retweetedQuackId, String createdAt) {
+        this.id = id;
         this.message = message;
+        this.userId = userId;
+        this.isReply = isReply;
+        this.repliedQuackId = repliedQuackId;
+        this.isRetweet = isRetweet;
+        this.retweetedQuackId = retweetedQuackId;
+        this.createdAt = createdAt;
     }
 
+    public Message() {
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getMessage() {
         return message;
@@ -18,5 +48,45 @@ public class Message {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public boolean isReply() {
+        return isReply;
+    }
+
+    public void setReply(boolean reply) {
+        isReply = reply;
+    }
+
+    public int getRepliedQuackId() {
+        return repliedQuackId;
+    }
+
+    public void setRepliedQuackId(int repliedQuackId) {
+        this.repliedQuackId = repliedQuackId;
+    }
+
+    public boolean isRetweet() {
+        return isRetweet;
+    }
+
+    public void setRetweet(boolean retweet) {
+        isRetweet = retweet;
+    }
+
+    public int getRetweetedQuackId() {
+        return retweetedQuackId;
+    }
+
+    public void setRetweetedQuackId(int retweetedQuackId) {
+        this.retweetedQuackId = retweetedQuackId;
     }
 }
--- demo/demo/src/main/java/com/soa/demo/web/indexController.java
+++ demo/demo/src/main/java/com/soa/demo/web/indexController.java
@@ -1,39 +1,161 @@
 package com.soa.demo.web;
 
 import com.soa.demo.objects.Message;
-import com.soa.demo.objects.User;
+import com.soa.demo.objects.UserData;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Controller;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.jdbc.core.JdbcTemplate;
 
-//import javax.websocket.EncodeException;
-//import java.io.IOException;
+import java.util.List;
+
 
 
 @RestController
 public class indexController {
 
-    @GetMapping("/{username}/messages/")
-    public String nothing(@PathVariable String username, Model model){
-        return "nothing";
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @GetMapping("/getQuacks")
+    public List<Message> getQuacks(Model model){
+        String sql = "SELECT * FROM quacks";
+        return jdbcTemplate.query(sql,
+                BeanPropertyRowMapper.newInstance(Message.class));
     }
 
-    @PostMapping("/{username}/postMessage")
-    public Message postMessage(@PathVariable String username, @RequestBody Message message){
+    //
+    @PostMapping("/postQuack")
+    public Message postMessage(@RequestBody Message message){
+        String sql = "INSERT INTO quacks (user_id, message, is_reply, reply_to_quack_id, is_retweet, retweet_to_quack_id) VALUES (?,?, ?, ?, ?, ?)";
 
+        int rows = jdbcTemplate.update(sql, message.getUserId(), message.getMessage(), message.isReply(), message.getRepliedQuackId(), message.isRetweet(), message.getRetweetedQuackId());
+        if (rows > 0) {
+            //If row has been created
+            System.out.println("A new row has been inserted.");
+        }
+        else {
+            //If row has not been created
+            System.out.println("Something went wrong.");
+        }
         return message;
     }
 
-    @PostMapping("addUser")
-    public User addUser(@RequestBody User user){
+    //Need id of user to remove follower from, decrements followers
+    @PostMapping("/removeFollower")
+    public int removeFollower(@RequestBody UserData message){
+        //postgres decrement follower value from userdata
+        String sql= "UPDATE userdata SET followers = followers - 1 WHERE username = ?";
+        int rows = jdbcTemplate.update(sql, message.getId());
+        if (rows > 0) {
+            //If row has been created
+            System.out.println("A new row has been inserted.");
+        }
+        else {
+            //If row has not been created
+            System.out.println("Something went wrong.");
+        }
+        return 0;
+    }
+    //Need id of user to remove follower from, increments followers
+    @PostMapping("/addFollower")
+    public int addFollower(@RequestBody UserData message){
+        //postgres decrement follower value from userdata
+        String sql= "UPDATE userdata SET followers = followers + 1 WHERE id = ?";
+        int rows = jdbcTemplate.update(sql, message.getId());
+        if (rows > 0) {
+            //If row has been created
+            System.out.println("A new row has been inserted.");
+        }
+        else {
+            //If row has not been created
+            System.out.println("Something went wrong.");
+        }
+        return 0;
+        //Add follower to userdata
+    }
+
+    //Need id of user to add follower to and id of follower to add
+    @PostMapping("/addFollowing")
+    public UserData addFollowing(@RequestBody UserData message){
+        //postgress add userID to array of followers in userdata
+        String sql = "INSERT INTO followings (user_id, following_id) VALUES (?,?)";
+
+        int rows = jdbcTemplate.update(sql, message.getId(), message.getFollowing());
+        if (rows > 0) {
+            //If row has been created
+            System.out.println("A new row has been inserted.");
+        }
+        else {
+            //If row has not been created
+            System.out.println("Something went wrong.");
+        }
+        return message;
+    }
+
+    //Need id of user to add tag and list of tags to add
+    @PostMapping("/addTag")
+    public UserData addTag(@RequestBody UserData message){
+        //postgress add userID to array of followers in userdata
+        String sql = "UPDATE userdata SET tags = array_append(tags, ?) WHERE id = ?";
+        int rows = jdbcTemplate.update(sql, message.getTags(), message.getId());
+        if (rows > 0) {
+            //If row has been created
+            System.out.println("A new row has been inserted.");
+        }
+        else {
+            //If row has not been created
+            System.out.println("Something went wrong.");
+        }
+        return message;
+    }
+
+    //Need id of user to add follower to and String of description to add
+    @PostMapping("/addDescription")
+    public UserData addDescription( @RequestBody UserData message){
+        //Change value of description in userdata for certain user.
+        String sql = "UPDATE userdata SET description = ? WHERE id = ?";
+
+        int rows = jdbcTemplate.update(sql, message.getDescription(), message.getId());
+        if (rows > 0) {
+            //If row has been created
+            System.out.println("A new row has been inserted.");
+        }
+        else {
+            //If row has not been created
+            System.out.println("Something went wrong.");
+        }
+        return message;
+    }
+
+    //Need id of user to add follower to and Username to add
+    @PostMapping("/addUser")
+    public UserData addUser(@RequestBody UserData user){
+        String sql = "INSERT INTO userdata (id, username, followers) VALUES (?,?, ?)";
+
+        int rows = jdbcTemplate.update(sql, user.getId(), user.getUsername(), 0);
+        if (rows > 0) {
+            //If row has been created
+            System.out.println("A new row has been inserted.");
+        }
+        else {
+            //If row has not been created
+            System.out.println("Something went wrong.");
+        }
         return user;
     }
 
+    //Get list of users
+    @GetMapping("/users")
+    public List<UserData>  getUsers(Model model){
+        String sql = "SELECT * FROM users";
+        List<UserData> listUsers = jdbcTemplate.query(sql,
+                BeanPropertyRowMapper.newInstance(UserData.class));
 
-
-
-
-
+        for (UserData user : listUsers) {
+            System.out.println(user);
+        }
+        return listUsers;
+    }
 }
